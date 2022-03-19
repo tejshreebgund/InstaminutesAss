abab0d1e7cf16fa93cd1e95a0423ca26f8a435cb
Tejshree Gund <tejubgund123@gmail.com>
2022-03-19 13:22:46 +0530

    first commit

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const schedule = require('node-schedule')
-function Trigger(str) {
-  let len = str.length;
-  let t= len* 1000;
+let i = 1
+function trigger(s) {
+    let t= len* 1000;
 
   //
   setTimeout(() => {
-    console.log(reverse(str));
+    console.log(reverse(s));
   }, t);
 }
 
 
 // this function take argument string reverse it return it
-function reverse(str) {
-  return str.split("").reverse().join("").toLowerCase();
+function reverse(s) {
+  return s.split("").reverse().join("").toLowerCase();
 }
  
  
-let i = 1
+
  const task = schedule.scheduleJob('*/2 * * * * *', () => { 
   
      if(i >=10) {
-        schedule.cancelJob(task)
+        schedule.cancelJob(task);
      }
-    console.log(`"text":${i} ksat`)
-    i++
-    console.log(`"date":`, new Date())
+    console.log(`"text":${i} ksat`);
+    i++;
+    console.log(`"date":`, new Date());
 
 })
 
